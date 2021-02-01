f6b25808c1061b0989e8a15657692d435f6d8401
poison_ivy <hellosvetasmirnova@gmail.com>
2021-02-02 01:49:25 +0300

    Исправила выдачу ошибок

--- js/main.js
+++ js/main.js
@@ -7,18 +7,27 @@ const getRandomFloat = (min, max) => {
 };
 
 
-const getRandonFixedFloat = (min, max, range = 1) => {
-  let errorMessage =  'Не корректное значение "от" или "до"';
-  if (min >= max || min < 0 || max < 0) {
-    throw new Error(errorMessage)
+const getRandomFixedFloat = (min, max, range = 1) => {
+  let errorMessage =  '';
+  if (min >= max) {
+    errorMessage = 'Значение "от" не может быть меньше чем "до"';
+  }
+  if (min < 0 || max < 0) {
+    errorMessage = 'Значения "от" и "до" не могут быть меньше нуля';
+  }
+  if (errorMessage) {
+    throw new Error(errorMessage);
   }
 
+
   const value = getRandomFloat(min, max);
-  return value.toFixed(range);
+  return Number(value.toFixed(range));
 };
 
 const getRandomNumber = (min, max) => {
-  return getRandonFixedFloat(min, max, 0);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return getRandomFixedFloat(min, max, 0);
 };
 
 
