6ecf3eabe062625bd81465abc1624c4a6ed96bbb
poison_ivy <hellosvetasmirnova@gmail.com>
2021-02-28 20:47:18 +0300

    Изменила название функций меняющих состояние страницы

--- js/filter.js
+++ js/filter.js
@@ -2,16 +2,16 @@ const mapFilteresForm = document.querySelector('.map__filters');
 const mapChildrenFilterestElements = mapFilteresForm.children;
 const mapFilterestElements = Array.from(mapChildrenFilterestElements);
 
-export const disableFilterMapForm = () => {
+export const disableFilter = () => {
   mapFilteresForm.classList.add('ad-form--disabled');
   mapFilterestElements.forEach((children) => {
-    children.setAttribute.disabled = true;
-  },
-  )};
+    children.disabled = true;
+  })
+};
 
-export const enableFilterMapForm = () => {
+export const enableFilter = () => {
   mapFilteresForm.classList.remove('ad-form--disabled');
   mapFilterestElements.forEach((children) => {
-    children.removeAttribute.disabled = false;
-  },
-  )};
+    children.disabled = false;
+  })
+};
