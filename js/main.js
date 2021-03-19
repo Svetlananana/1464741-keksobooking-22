2bd5edb6b5fb7212a50bf6d5fe3c299c4d5d2782
poison_ivy <hellosvetasmirnova@gmail.com>
2021-03-19 16:25:29 +0300

    Добавила обработчик на reset

--- js/main.js
+++ js/main.js
@@ -1,26 +1,43 @@
-import { initializeForm, addSubmitHandler, resetForm, setCapacityValue } from './form.js';
+/* global _:readonly */
+
+import { initializeForm, addSubmitHandler, resetForm, setCapacityValue, addResetHandler } from './form.js';
 import { renderMarkers, initializeMap } from './map.js';
 import { disablePage, enablePage } from './page.js';
 import { getOffers } from './backend.js';
+import { addFilterHandler, resetFilter } from './filter.js';
 
+const DEBOUNCE_TIME = 500;
+
+disablePage();
+initializeForm();
 
 getOffers()
   .then((offers) => {
-    disablePage();
-
-    initializeForm();
-
     initializeMap(() => {
       enablePage();
+      renderMarkers(offers);
     });
 
-    renderMarkers(offers);
-
     addSubmitHandler(() => {
+      resetFilter();
       resetForm();
       initializeMap();
       setCapacityValue();
+      renderMarkers(offers);
     });
+
+    addResetHandler(() => {
+      resetFilter();
+      resetForm();
+      initializeMap();
+      setCapacityValue();
+      renderMarkers(offers);
+    });
+
+    addFilterHandler(
+      _.debounce(renderMarkers, DEBOUNCE_TIME),
+      offers,
+    );
   });
 
 
